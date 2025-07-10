0780a503564b5e59038852b401fb4d04fa4ca475
yubinex <janhauck99@gmail.com>
2025-07-10 08:33:30 +0200

    create player scene and attach script

---
+++ player.gd
@@ -0,0 +1,13 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	print("Hello World!")
+	print("Don't panic")
+	print("42")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
