4a8f49ac885e47bd13d3552c13d5646253937668
yubinex <janhauck99@gmail.com>
2025-07-11 00:26:13 +0200

    feat: Add player node with basic functionality
    - Added a new `new_var` integer variable initialized to 0.
    - Modified the `_ready` function to print a single number instead of multiple strings.
    - Updated the `_process` function to check for spacebar input and increment the `new_var` if pressed.

--- player.gd
+++ player.gd
@@ -1,13 +1,18 @@
 extends Node3D
 
 
+var new_var: int = 0
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print("Hello World!")
-	print("Don't panic")
-	print("42")
+	var my_number: int = 10
+	print(my_number)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_just_pressed("ui_accept"):
+		print("spacebar was pressed")
+		new_var += 10
+		print(new_var)
