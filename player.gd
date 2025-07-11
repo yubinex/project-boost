a77f54936c5ce400ba51281583976488ad58ad72
yubinex <janhauck99@gmail.com>
2025-07-11 02:01:32 +0200

    fix: Improved player movement and environment rendering
    -
    
    - Updated the player's _process function to use the "ui_accept" action for spacebar presses.
    - Added delta-based movement for the player when the spacebar is pressed.
    - Implemented rotation for the player using the left and right arrow keys.
    - Improved environment rendering by loading the scene with more steps (load_steps=6) and format (format=3).
    - Set up a procedurally generated sky, terrain, and environment.
    - Added a directional light source to provide better lighting in the scene.

--- player.gd
+++ player.gd
@@ -1,18 +1,11 @@
 extends Node3D
 
 
-var new_var: int = 0
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	var my_number: int = 10
-	print(my_number)
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("ui_accept"):
-		print("spacebar was pressed")
-		new_var += 10
-		print(new_var)
+	if Input.is_action_pressed("ui_accept"):
+		position.y += delta
+	if Input.is_action_pressed("ui_left"):
+		rotate_z(delta)
+	if Input.is_action_pressed("ui_right"):
+		rotate_z(-delta)
