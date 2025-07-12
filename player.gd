c32c562c8885c13e2de7a514521b15d6f2338a11
yubinex <janhauck99@gmail.com>
2025-07-12 06:16:24 +0200

    feat: Add player thrust and torque configuration options
    - Added `thrust` property to configure the amount of vertical force applied when moving.
    - Added `torque_thrust` property to control the amount of torque applied during left and right input actions.
    - Updated `_process` function to use these new properties instead of hardcoded values.
    - Improved code readability by replacing magic numbers with named variables.

--- player.gd
+++ player.gd
@@ -1,16 +1,20 @@
 extends RigidBody3D
 
+## How much vertical force to apply when moving.
+@export_range(750.0, 3000.0) var thrust: float = 1000.0 
+@export var torque_thrust: float = 100.0
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("boost"):
-		apply_central_force(basis.y * delta * 1000.0)
+		apply_central_force(basis.y * delta * thrust)
 
 	if Input.is_action_pressed("left"):
-		apply_torque(Vector3(0.0, 0.0, 100.0 * delta))
+		apply_torque(Vector3(0.0, 0.0, torque_thrust * delta))
 
 	if Input.is_action_pressed("right"):
-		apply_torque(Vector3(0.0, 0.0, -100.0 * delta))
+		apply_torque(Vector3(0.0, 0.0, -torque_thrust * delta))
 
 
 func _on_body_entered(body: Node) -> void:
